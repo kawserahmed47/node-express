7f92e5d2e3d8811e51726aa50b4a217b97d7ac59
Kawser Ahmed <kawser@doodleinc.net>
2021-04-10 13:01:48 +0600

    first assignment

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const bodyParser = require('body-parser');
 const http = require('http');
 
 
-
+const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+
 app.use(express.static(__dirname+'/public'));
 app.use((req, res, next) => {
     console.log(req.headers);
